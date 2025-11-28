289cc64f686332fb61780bffa553cbb5c3e6e2b0
azam yest tsar <akbarashurov2702@gmail.com>
2025-11-28 16:30:38 +0400

    added new handlers

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import telebot # библиотека telebot
 from config import token # импорт токена
+import random
+import time
+from datetime import datetime
 
 bot = telebot.TeleBot(token) 
 
@@ -37,6 +40,33 @@ def ban_user(message):
             bot.ban_chat_member(chat_id, user_id)
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
 
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+
+@bot.message_handler(commands=['dice'])
+def roll_dice(message):
+    dice_message = bot.send_dice(message.chat.id)
+    time.sleep(4)
+    result = dice_message.dice.value
+    bot.reply_to(message, f"Выпало число: **{result}**!")
+
+
+# 6. Хендлер для команды /coin (Монетка)
+@bot.message_handler(commands=['coin'])
+def flip_coin(message):
+    result = random.choice(["Орел", "Решка"])
+    bot.reply_to(message, f"Монетка подброшена... Выпало: **{result}**!")
+
+
+# 7. Хендлер для команды /time
+@bot.message_handler(commands=['time'])
+def get_time(message):
+    current_time = datetime.now().strftime("%H:%M:%S")
+    bot.reply_to(message, f"Текущее время: **{current_time}**")
 
 
 bot.infinity_polling(none_stop=True)
